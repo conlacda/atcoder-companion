a69b7d377682ba8c21dec2a38bf00c7a980b31eb
conlacda <phamlong15297@gmail.com>
2024-04-30 12:52:35 +0900

    Create testcase.spec.ts

---
+++ browser-extension/chrome-extension-test/tests/testcase.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from '../fixtures';
+import { Page, Locator } from "@playwright/test";
+
+require('dotenv').config({ path: './.env' });
+
+// chromium.launch PersistentContext prevents you from using multiple pages, so do not use parallel mode.
+// The parallel mode opens each tab for each test, and the context will be closed after one test.
+test.describe.configure({ mode: 'serial' });
+
+let page: Page;
+
+test.beforeAll(async ({ context }) => {
+    page = await context.newPage();
+});
+
+test.afterAll(async () => {
+    await page.close();
+});
+
+test('More test cases should be added to problem statement.', async () => {
+    await page.goto('https://atcoder.jp/contests/abc347/tasks/abc347_a');
+    const taskStatment: Locator = page.locator('#task-statement');
+    for (let i = 1; i <= 12; i++) {
+        await expect(taskStatment).toContainText(`Test Input ${i}`);
+        await expect(taskStatment).toContainText(`Test Output ${i}`);
+    }
+});
