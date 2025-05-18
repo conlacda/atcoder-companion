db0a6e662bbf5bb43cfa8d075a23e8f9df45235f
conlacda <phamlong15297@gmail.com>
2025-05-18 20:25:49 +0900

    version 0.3.3

--- browser-extension/chrome-extension/scripts/background.js
+++ browser-extension/chrome-extension/scripts/background.js
@@ -1,5 +1,5 @@
 chrome.runtime.onInstalled.addListener((details) => {
-    if (details.previousVersion <= '0.3.1' && details.reason === 'update') {
+    if (details.previousVersion <= '0.3.2' && details.reason === 'update') {
         chrome.storage.local.clear();
     }
 });
